c11ade3cf1c15ef10b4b35bf72462fa72a31a3f4
Max Besseling <maxb@MacBook-Air-van-Max.local>
2026-05-09 15:51:00 +0200

    Changed files: renamed "Exersice" to what the book suggested

--- Chapter_01/full_name.py
+++ Chapter_01/full_name.py
@@ -1,4 +1,5 @@
 first_name = "ada"
 last_name = "lovelace"
-full_name = f"{first_name} {last_name}" 
+full_name = f"{first_name} {last_name}" # f-string, the f stands for format 
 # to insert a variable's value into a string, place the letter f immediately before the opening quotation mark
+print(f"Hello, {full_name.title()}!")
