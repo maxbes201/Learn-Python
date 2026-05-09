9944820319413b7d76333cda0c089cf5f6afa911
Max Besseling <maxb@MacBook-Air-van-Max.local>
2026-05-09 15:43:08 +0200

    Added: Exercise03.py

---
+++ Chapter_01/Excersise03.py
@@ -0,0 +1,4 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}" 
+# to insert a variable's value into a string, place the letter f immediately before the opening quotation mark
--- Chapter_01/Exercise02.py
+++ Chapter_01/Exercise02.py
@@ -1,2 +1,4 @@
 name = "ada lovelace"
 print(name.title()) # string with method .title(), makes the first letter uppercase
+print(name.upper()) # string witb method .upper(), makes the full string in uppercase
+print(name.lower()) # string with method .lower(), najes the full string in lowercase
