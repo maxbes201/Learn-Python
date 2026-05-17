e40f5cb5a1d9a3fe239ef8294a9d9bd150b94a3f
Max Besseling <maxb@unknown3ae74759e30b.domain_not_set.invalid>
2026-05-17 14:08:57 +0200

    Finished full_name.py

--- Chapter_01/full_name.py
+++ Chapter_01/full_name.py
@@ -2,4 +2,5 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}" # f-string, the f stands for format 
 # to insert a variable's value into a string, place the letter f immediately before the opening quotation mark
-print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}"
+print(message)
